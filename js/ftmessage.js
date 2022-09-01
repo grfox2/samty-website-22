f6bd6ea25a861043a83eaf64cdaf4a0e492e8a43
Gerardo Ramon <feliperam10@gmail.com>
2022-09-01 00:41:40 -0500

    GA updates.

--- js/ftmessage.js
+++ js/ftmessage.js
@@ -2,7 +2,7 @@ let cookiemsg = document.getElementById('cookiemsg');
 
 // Show cookie message for first time user.
 if (document.cookie.indexOf("accepted" + '=true') > -1) {
-	window['ga-disable-GA_MEASUREMENT_ID'] = false;
+	window['ga-disable-UA-17305443-1'] = false;
 } else {
 	cookiemsg.setAttribute("class", "alert alert-dark alert-dismissible")
 }
